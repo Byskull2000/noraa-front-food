5a232ab071c4ea04cba385564ed2d0b112e33a73
Pablo <pablito.hans@hotmail.com>
2024-06-19 12:44:22 -0400

    Correcion de rutas

--- src/app/mapas/MapPanel.tsx
+++ src/app/mapas/MapPanel.tsx
@@ -122,41 +122,6 @@ const MapPanel: React.FC<MapPanelProps> = ({ centro }: MapPanelProps) => {
             </Marker>
           ))}
         </MapContainer>
-
-        {showDetail && restauranteSeleccionado && (
-          <div
-            style={{
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              right: 0,
-              backgroundColor: 'white',
-              padding: '1em',
-              boxShadow: '0 -2px 10px rgba(0, 0, 0, 0.1)',
-              borderTopLeftRadius: '10px',
-              borderTopRightRadius: '10px',
-              zIndex: 1000,
-            }}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <a href="paginaRestaurante" key={restauranteSeleccionado.id_restaurante} 
-              onClick={() => handleCardClick(restauranteSeleccionado.id_restaurante)} // Attach click event handler
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 sm:items-start sm:place-items-start" style={{ textDecoration: 'none', margin: '1%', width: '48%', maxWidth: '48%' }}>
-                {restauranteSeleccionado.imagen && (
-                  <ImgConstructor
-                    imgBytea={restauranteSeleccionado.imagen}
-                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                    className="rounded-t-lg h-80 sm:h-auto sm:w-48 sm:rounded-none sm:rounded-l-lg max-w-full"
-                  />
-                )}
-                <div className="flex flex-col p-4 leading-normal text-left sm:text-left mr-12" style={{ width: '48%' }}>
-                  <h5 className="mb-2 lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white text-sm">{restauranteSeleccionado.nombre_restaurante}</h5>
-                  <p className="mt-1 font-normal text-xs lg:text-sm text-gray-700 text-left">Hora de entrada: {restauranteSeleccionado.horario_atencion}</p>
-                  <p className="mt-1 mb-1 font-normal text-xs sm:text-sm text-gray-700 flex items-center text-left">Calificación: {restauranteSeleccionado.valoracion} <StarIcon /></p>
-                </div>
-            </a>
-          </div>
-        )}
       </div>
     </div>
   );
