9cab06e2e1466faad985fe80052c9e7ace0e08d6
Pablo <pablito.hans@hotmail.com>
2024-06-19 01:37:44 -0400

    Correcion de ruteos

--- src/app/principal/Body.tsx
+++ src/app/principal/Body.tsx
@@ -12,9 +12,6 @@ import { useRouter } from 'next/router';
 const Body = () => {
     const router = useRouter();
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const userLocationLat = useUserLocation()?.lat;
-    const userLocationLng = useUserLocation()?.lng;
-    const { direccion } = useGeocoder({ lat: userLocationLat ? userLocationLat : 0, lng: userLocationLng ? userLocationLng : 0 });
 
     const openModal = () => {
         setModalIsOpen(true);
@@ -39,7 +36,6 @@ const Body = () => {
                         <path fill="#ffffff" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
                     </svg>
-                    <span className="text-sm lg:text-md">{direccion}</span>
                 </button>
             </div>
 
@@ -72,7 +68,6 @@ const Body = () => {
                 >
                     Volver
                 </button>
-                <MapPanel centro={[(userLocationLat != null) ? userLocationLat : 0.0, (userLocationLng != null) ? userLocationLng : 0.0]} />
             </Modal>
 
             <div className="lg:w-full">
@@ -86,7 +81,6 @@ const Body = () => {
                             </button>
                         </div>
                         <h1 className="lg:font-bold lg:ml-8 lg:mt-11 xl:text-2xl font-bold mt-5 ml-3 text-lg">Negocios cercanos a ti</h1> 
-                        <DashboardCercanos centro={[(userLocationLng != null) ? userLocationLng : 0.0, (userLocationLat != null) ? userLocationLat : 0.0]} /> 
                 
                     </>
                 )}
